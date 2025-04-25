87ff7a2c25b2a8ea6837f3a94af5d6aaa11d0e2a
wilsongayo <wilsongayo@golden-success.com>
2025-04-25 08:11:32 +0000

    updated sequelize

--- _helpers/db.js
+++ _helpers/db.js
@@ -9,11 +9,21 @@ initialize();
 async function initialize() {
     // create db if it doesn't already exist
     const { host, port, user, password, database } = config.database;
+    
     const connection = await mysql.createConnection({ host, port, user, password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    // const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(
+        database,
+        user,
+        password,
+        {
+           host: host,
+           dialect: 'mysql'
+        }
+       );
 
     // init models and add them to the exported db object
     db.Account = require('../accounts/account.model')(sequelize);
@@ -36,5 +46,5 @@ async function initialize() {
     db.Workflow.belongsTo(db.Employee, { foreignKey: 'employeeId', as: 'employee' });
 
     // sync all models with database
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ alter: false });
 }
